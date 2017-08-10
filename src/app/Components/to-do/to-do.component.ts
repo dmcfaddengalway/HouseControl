80f3959522cc2af340715afb55d69b0296dcdca9
Daniel McFadden <dmcfaddengalway@gmail.com>
2017-08-10 19:58:53 -0400

    SCSS Style, toDo Work
    added elements to ToDo page, styled toDo page, set color scheme,
    imported new scss variable

--- src/app/Components/to-do/to-do.component.ts
+++ src/app/Components/to-do/to-do.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.scss']
+  styleUrls: ['./to-do.component.scss', '../../Assets/CSS/_variables.scss']
 })
 export class ToDoComponent implements OnInit {
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './Assets/CSS/_variables.scss']
 })
 
 export class AppComponent { }
