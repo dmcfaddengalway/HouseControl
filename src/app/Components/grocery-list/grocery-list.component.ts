845025316bad421ff7fcb5089783d0b3fec605c2
Daniel McFadden <dmcfaddengalway@gmail.com>
2017-08-14 18:18:01 -0400

    added new functions for clearing functionality, added styled checkbox, and checkbox element

--- src/app/Components/grocery-list/grocery-list.component.ts
+++ src/app/Components/grocery-list/grocery-list.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroceryListComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
 
-  ngOnInit() {
-  }
+    }
+
+    ngOnInit() {
+
+    }
+
+    clearList() {
+
+    }
+
+    clearAllList() {
+
+    }
 
 }
