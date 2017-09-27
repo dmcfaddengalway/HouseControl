df25774c3e75a937432a89eae381e02887b39fc3
Daniel McFadden <dmcfaddengalway@gmail.com>
2017-09-27 01:23:13 +0100

    added sharedStyles calsses, made wireframe of content

--- src/app/Components/cleaning/cleaning.component.ts
+++ src/app/Components/cleaning/cleaning.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cleaning',
   templateUrl: './cleaning.component.html',
-  styleUrls: ['./cleaning.component.scss']
+  styleUrls: ['./cleaning.component.scss', '../../Assets/CSS/sharedStyles.scss']
 })
 export class CleaningComponent implements OnInit {
 
