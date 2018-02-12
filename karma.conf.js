fde4251e869aead6b503487079ddfaa5f250bda2
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-02-12 21:20:44 +0000

    added firefox to plugin

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,9 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
